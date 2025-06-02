2485e97cb25a0ea5804bfc1f6d0fe5da5fce61b5
johann <64203794+johann-DP@users.noreply.github.com>
2025-06-02 10:07:14 +0200

    Add LSTM training utilities

--- pred/__init__.py
+++ pred/__init__.py
@@ -6,6 +6,13 @@ from .aggregate_revenue import (
     build_timeseries,
 )
 from .preprocess_timeseries import preprocess_series, preprocess_all
+from .lstm_forecast import (
+    create_lstm_sequences,
+    scale_lstm_data,
+    build_lstm_model,
+    train_lstm_model,
+    quick_predict_check,
+)
 
 __all__ = [
     "load_won_opportunities",
@@ -13,4 +20,9 @@ __all__ = [
     "build_timeseries",
     "preprocess_series",
     "preprocess_all",
+    "create_lstm_sequences",
+    "scale_lstm_data",
+    "build_lstm_model",
+    "train_lstm_model",
+    "quick_predict_check",
 ]
