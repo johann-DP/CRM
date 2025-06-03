bd1dbec3a6a24b89004fb430390d06f282a86ab9
johan <johann.grisel@datapredict.org>
2025-06-03 16:32:12 +0200

    backup env

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# Installe les dépendances en local (avant que le réseau ne soit coupé)
-python -m pip install --upgrade pip
-python -m pip install --no-cache-dir -r requirements.txt
-python -m pip install --no-cache-dir pytest
-# éventuellement : python -m pip install --no-cache-dir -e .
+python -m pip install -r requirements.txt
