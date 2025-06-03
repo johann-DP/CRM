5f915946f88e38d21254f54fdc6614b18245f0d8
johan <johann.grisel@datapredict.org>
2025-06-03 13:10:35 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 # Setup script run by Codex before network access is disabled.
 # Installs the Python dependencies listed in requirements.txt
 
+python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+python -m pip install pytest
