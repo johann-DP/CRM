4f835e47fad3cdb89356b16ebdba8a4e1166608d
johan <johann.grisel@datapredict.org>
2025-06-03 15:58:38 +0200

    Rendre setup.sh exécutable

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+# Installe les dépendances en local (avant que le réseau ne soit coupé)
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip install --no-cache-dir -r requirements.txt
+python -m pip install --no-cache-dir pytest
+# éventuellement : python -m pip install --no-cache-dir -e .
