419457464e85d0adac6b11d4f40a9e91c74b251b
johan <johann.grisel@datapredict.org>
2025-06-03 14:47:11 +0200

    update Codex env

--- setup.sh
+++ setup.sh
@@ -4,4 +4,3 @@ python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
 python -m pip install --no-cache-dir pytest
 python -m pip install --no-cache-dir -e .
-
