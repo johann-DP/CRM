2125030c2af841b93573210b8e351af4b3cacac1
johan <johann.grisel@datapredict.org>
2025-06-03 14:17:40 +0200

    Rendre setup.sh exécutable

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
 python -m pip install --no-cache-dir pytest
 python -m pip install --no-cache-dir -e .
+
